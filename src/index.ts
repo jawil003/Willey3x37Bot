9bcbda1bdb14dd0f78c807f0111192ae25a081ac
Jannik <jw19980309@gmail.com>
2020-10-23 11:35:57 +0200

    Added twitch-webhooks

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import tmi from "tmi.js";
+import { ApiClient } from "twitch";
+import { ClientCredentialsAuthProvider } from "twitch-auth";
+import { SimpleAdapter, WebHookListener } from "twitch-webhooks";
 import { config } from "dotenv";
 import CommandsService from "./services/commands.service";
 config();
 export let client: tmi.Client;
+export let webhook: WebHookListener;
 export const channel = process.env.CHANNEL as string;
 (async () => {
+  const clientId = process.env.CLIENT_ID as string;
   client = tmi.Client({
-    connection: { reconnect: true, secure: true },
+    connection: { reconnect: true, secure: true, port: 8089 },
     options: { debug: true },
     identity: {
       username: process.env.USERNAME,
@@ -14,8 +19,23 @@ export const channel = process.env.CHANNEL as string;
     },
     channels: ["#" + channel],
   });
-
   await client.connect();
 
+  const authProvider = new ClientCredentialsAuthProvider(
+    clientId,
+    process.env.CLIENT_SECRET as string
+  );
+  const apiClient = new ApiClient({ authProvider, logLevel: 7 });
+  webhook = new WebHookListener(
+    apiClient,
+    new SimpleAdapter({
+      hostName: "http://localhost",
+      listenerPort: 8090,
+    })
+  );
+  webhook.listen();
+
   CommandsService.init();
+
+  webhook.subscribeToFollowsToUser(process.env.USERNAME as string, () => {});
 })();
